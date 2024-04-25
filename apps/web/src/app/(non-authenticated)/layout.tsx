56f9e7b7a5047c633a41239ac07445a8254227e5
kaqons <123312657+kaqons@users.noreply.github.com>
2024-04-24 21:13:58 -0700

    Delete apps/web/src/app/(non-authenticated) directory

--- apps/web/src/app/(non-authenticated)/layout.tsx
+++
@@ -1,8 +0,0 @@
-'use client'
-
-import { AuthenticationGuard } from '@web/modules/authentication'
-import { ReactNode } from 'react'
-
-export default function LoginLayout({ children }: { children: ReactNode }) {
-  return <AuthenticationGuard isPublic={true}>{children}</AuthenticationGuard>
-}
